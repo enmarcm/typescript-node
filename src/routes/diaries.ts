26803bb17198cf3a4b7eefd0ff751307521451db
enmarcm <theenmanuel123@gmail.com>
2024-02-19 22:11:08 -0400

    findById

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -8,6 +8,15 @@ diariesRouter.get("/", (_req, res) => {
   return res.send(diaryServices.getEntriesWithoutSensitiveInfo());
 });
 
+diariesRouter.get("/:id", (req, res) => {
+  const diary = diaryServices.findById(Number(req.params.id));
+  if (diary) {
+    return res.send(diary);
+  } else {
+    return res.status(404).send("Diary not found");
+  }
+});
+
 diariesRouter.get("/", (_req, res) => {
   return res.send("Saving a new entry diary");
 });
--- src/services/diariesServices.ts
+++ src/services/diariesServices.ts
@@ -5,6 +5,9 @@ const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>;
 
 export const getEntries = () => diaries;
 
+export const findById = (id: number): DiaryEntry | undefined =>
+  diaries.find((d) => d.id === id);
+
 export const getEntriesWithoutSensitiveInfo =
   (): Array<NonSensitiveInfoDiaryEntry> =>
     diaries.map(({ id, date, visibility, weather }) => ({
