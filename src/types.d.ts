38924ddbb2bef1e8998f3d8aa29efd798a51854f
enmarcm <theenmanuel123@gmail.com>
2024-02-26 11:32:13 -0400

    si

--- src/types.d.ts
+++ src/types.d.ts
@@ -16,3 +16,5 @@ export type NewDiaryEntry = Omit<DiaryEntry, "id">;
 
 // export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, "id" | "date" | "weather">
 export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, "comment">;
+
+//Cambio
