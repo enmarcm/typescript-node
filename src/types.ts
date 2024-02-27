a71a4d5d9cabcef0c752f590338ee5eb34b9af41
enmarcm <theenmanuel123@gmail.com>
2024-02-27 00:02:13 -0400

    changes

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,9 @@
-export type Weather = "sunny" | "rainy" | "cloudy" | "windy" | "stormy";
-
-export type Visibility = "great" | "good" | "ok" | "poor";
-
 export type DateParse = `${number}-${number}-${number}`;
 
 export interface DiaryEntry {
   id: number;
   date: DateParse;
-  weather: Wheater;
+  weather: Weather;
   visibility: Visibility;
   comment: string;
 }
@@ -16,3 +12,18 @@ export type NewDiaryEntry = Omit<DiaryEntry, "id">;
 
 // export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, "id" | "date" | "weather">
 export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, "comment">;
+
+export enum Weather{
+  Sunny = "sunny",
+  Rainy = "rainy",
+  Cloudy = "cloudy",
+  Windy = "windy",
+  Stormy = "stormy"
+}
+
+export enum Visibility{
+  Great = "great",
+  Good = "good",
+  Ok = "ok",
+  Poor = "poor"
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,56 @@
-import { NewDiaryEntry } from "./types";
+import { NewDiaryEntry, Weather, DateParse, Visibility } from "./types";
+
+const parseComment = (commentFromRequest: any): string => {
+  if (!isString(commentFromRequest) || !commentFromRequest) {
+    throw new Error("Incorrect or missing comment");
+  }
+
+  return commentFromRequest;
+};
+
+const parseDate = (date: any): DateParse => {
+  if (!isString(date) || !isDate(date)) {
+    throw new Error("Incorrect or missing date: " + date);
+  }
+
+  return date;
+};
+
+const parseWeather = (weather: any): Weather => {
+  if (!isString(weather) || !isWeather(weather)) {
+    throw new Error("Incorrect or missing weather: ");
+  }
+
+  return weather;
+};
+
+const parseVisibility = (visibility: any): Visibility => {
+  if (!isString(visibility) || !isVisibility(visibility)) {
+    throw new Error("Incorrect or missing visibility: ");
+  }
+
+  return visibility;
+};
+
+const isWeather = (param: any): boolean =>
+  Object.values(Weather).includes(param);
+
+const isString = (string: string): boolean => typeof string === "string";
+
+const isDate = (date: string): boolean => Boolean(Date.parse(date));
+
+const isVisibility = (param: any): boolean =>
+  Object.values(Visibility).includes(param);
 
 const toNewDiaryEntry = (object: any): NewDiaryEntry => {
-    const newEntry: NewDiaryEntry = {}
+  const newEntry: NewDiaryEntry = {
+    comment: parseComment(object?.comment),
+    date: parseDate(object?.date),
+    weather: parseWeather(object?.weather),
+    visibility: parseVisibility(object?.visibility),
+  };
+
+  return newEntry;
 };
 
 export default toNewDiaryEntry;
